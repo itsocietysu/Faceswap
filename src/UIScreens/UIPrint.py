333b56fc008d29c36a1b1d194cb4e7e1259430fc
chironovaa <chironovaa@mail.ru>
2018-07-12 18:29:51 +0300

    Fixed implemented filter selection

--- src/UIScreens/UIPrint.py
+++ src/UIScreens/UIPrint.py
@@ -20,10 +20,7 @@ class UIPrint(UIScreen):
         return Filtering().filter(img, self._parent.filter[self._parent.filter_id])
 
     def create_photo(self, name, img):
-        #filtering = Filtering()
         cv2.imwrite(name, self.filtering(img))
-        #for _ in self._parent.filter:
-        #    cv2.imwrite('./filter_results/tmp_to_print_%s.png' % _['name'], filtering.filter(img, _))
 
     def print_photo(self, obj):
         os.system("lp %s" % self.TEMP_NAME)
@@ -34,7 +31,18 @@ class UIPrint(UIScreen):
         cam_test = pygame.transform.smoothscale(pygame.image.load("./sprites/camtest.png"),
                                                 (self._parent.w, self._parent.h))
 
-        self.elements["camera"] = UISprite(self.surface, cam_test, 0, 0, self, 0)
+        self.elements["camera"] = UISprite(self.surface,
+                                           cam_test,
+                                           0,
+                                           0,
+                                           self,
+                                           0)
+
+        # Back button
+        def click_exit(btn):
+           self.shoot = False
+           self.parent.buttonClicked(btn)
+
 
         button = pygame.image.load("./sprites/back.png")
         buttonHov = pygame.image.load("./sprites/back_hov_b.png")
@@ -46,11 +54,17 @@ class UIPrint(UIScreen):
                                                + button.get_rect().width,
                                                self.surface.get_rect().bottom - button.get_rect().height,
                                                {
-                                                   'click': self.parent.buttonClicked
+                                                   'click': click_exit
+                                                   #self.parent.buttonClicked
                                                },
                                                self,
                                                1)
 
+
+
+
+
+        # Print button
         button = pygame.image.load("./sprites/print.png")
         buttonHov = pygame.image.load("./sprites/print_hov_b.png")
         self.elements["print"] = UISpriteButton("print",
@@ -82,7 +96,6 @@ class UIPrint(UIScreen):
         i = 0
         base_layer = 2
         self.max_layer = len(ids) + 2 + base_layer
-
         SCALE = 1.2
         DELAY = 0.2
 
@@ -111,34 +124,26 @@ class UIPrint(UIScreen):
             self._parent.filter_id = int(btn.name.split('_')[1]) - 1
 
         for idx in ids:
-            sc_a = pygame.transform.smoothscale(pygame.image.load("./sprites/filters/%d.png" % idx),
-                                                (w, h))
-            sc_d = pygame.transform.smoothscale(pygame.image.load("./sprites/filters/%d_d.png" % idx),
-                                                (w, h))
-
-            self.elements["icon_%d" % idx] = \
-                UISpriteButton("icon_%d" % idx,
-                               self.surface,
-                               sc_d,
-                               sc_a,
-                               l,
-                               t + (h + v_offset) * i,
-                               {
-                                   'click': on_icon_click,
-                                   'hoverin': insert_tween_in,
-                                   'hoverout': insert_tween_out,
-                               },
-                               self,
-                               base_layer + idx)
-
+            sc_a = pygame.transform.smoothscale(pygame.image.load("./sprites/filters/%d.png" % idx), (w, h))
+            sc_d = pygame.transform.smoothscale(pygame.image.load("./sprites/filters/%d_d.png" % idx), (w, h))
+            self.elements["icon_%d" % idx] = UISpriteButton("icon_%d" % idx,
+                                                            self.surface,
+                                                            sc_d,
+                                                            sc_a,
+                                                            l,
+                                                            t + (h + v_offset) * i,
+                                                            {
+                                                                'click': on_icon_click,
+                                                                'hoverin': insert_tween_in,
+                                                                'hoverout': insert_tween_out,
+                                                            },
+                                                            self,
+                                                            base_layer + idx)
             i += 1
 
     def transition(self, btn=None):
-
         is_in = btn == None
-
         from_left = ["left_backpanel", "icon_1", "icon_2", "icon_3", "icon_4", "icon_5"]
-
         DX = 300
         DY = 300
         DELAY = 0.2
@@ -148,10 +153,9 @@ class UIPrint(UIScreen):
             if is_in and elem.startswith("icon"):
                 btn_idx = int(elem.split('_')[1])
 
-                if btn_idx == self._parent.storage.current_scientist_id:
+                if btn_idx == self._parent.filter_id:
                     self.elements[elem].layer = self.max_layer - 1
                     self.elements[elem].insertTween(UIBumpEffect(1.2, DELAY, True))
-
 
         def delayedCallback():
             self.parent.buttonClicked(btn)
@@ -161,7 +165,6 @@ class UIPrint(UIScreen):
             t.start()
 
     def draw(self):
-
         self.create_photo(self.TEMP_NAME, self.img)
         self.frame = pygame.image.load(self.TEMP_NAME)
         self.elements["camera"].image = pygame.transform.smoothscale(self.frame, (self._parent.w, self._parent.h))
@@ -169,7 +172,6 @@ class UIPrint(UIScreen):
 
     def setVisible(self, val, params=()):
         UIScreen.setVisible(self, val, params)
-
         if val:
             if (self.shoot == False):
                 camera = self.parent.camera
@@ -179,7 +181,6 @@ class UIPrint(UIScreen):
                 self.img = swapper.apply_effect(camera.get_last_shot())
                 self.shoot = True
             self.transition()
-
             self.create_photo(self.TEMP_NAME, self.img)
             self.frame = pygame.image.load(self.TEMP_NAME)
             self.elements["camera"].image = pygame.transform.smoothscale(self.frame, (self._parent.w, self._parent.h))
